eb581304ccdd437aa8aaf25ed58ece4e1c6ac987
WarriorGod-Bro <warriorbrocoding11511@outlook.com>
2025-04-17 16:46:12 -0500

    add initial game implementation and .gitignore file

---
+++ Game.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+Author : WarriorBro 
+Projetc name : Guess the number game 
+Project language : C language
+Project Level : beginner
+Purpose : To help beginners to develope c language project.
+Description : The game that generate a secret number between the 1 to 1000 and say user to find that number, this game shows how many attempts user takes to find the number.
+*/
+
+int main(){
+    
+
+    return 0;
+}
